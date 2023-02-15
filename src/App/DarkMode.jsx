50b951c54021a1cf12d990692bfa9ddcc3c00d73
Adriana Gonzaga <adrianagonzaga@Adrianas-iMac.fritz.box>
2023-02-15 17:10:29 +0100

    changed button dark mode

--- src/App/DarkMode.jsx
+++ src/App/DarkMode.jsx
@@ -25,12 +25,28 @@ function DarkMode() {
   }
 
   return (
-    <div className="style-icon-dark-mode">
-      <img
-        src={darkModeisOn === true ? DarkModeOn : DarkModeOff} // ternary operator => boolean ? if true : if false
-        onClick={onChangeDarkMode}
-      />
-      <div className="dark-mode-name">{modeName}</div>
+    // <div className="style-icon-dark-mode">
+    //   <img
+    //     src={darkModeisOn === true ? DarkModeOn : DarkModeOff} // ternary operator => boolean ? if true : if false
+    //     onClick={onChangeDarkMode}
+    //   />
+    //   <div className="dark-mode-name">{modeName}</div>
+    // </div>
+    <div class="toggleWrapper">
+      <input type="checkbox" class="dn" id="dn" onClick={onChangeDarkMode} />
+      <label for="dn" class="toggle">
+        <span class="toggle__handler">
+          <span class="crater crater--1"></span>
+          <span class="crater crater--2"></span>
+          <span class="crater crater--3"></span>
+        </span>
+        <span class="star star--1"></span>
+        <span class="star star--2"></span>
+        <span class="star star--3"></span>
+        <span class="star star--4"></span>
+        <span class="star star--5"></span>
+        <span class="star star--6"></span>
+      </label>
     </div>
   );
 }
