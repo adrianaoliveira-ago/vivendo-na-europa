ecab076163c337b334db231273a8b832fdff7da9
Adriana Gonzaga <adrianagonzaga@Adrianas-iMac.fritz.box>
2023-03-09 17:56:50 +0100

    added a new boxes

--- src/App/AffiliateCard.jsx
+++ src/App/AffiliateCard.jsx
@@ -1,23 +1,42 @@
 // import Header from "./Header";
 // import Footer from "./Footer";
 import "./AffiliateCard.css";
+import OrangeTag from "./affiliatelinkTag.svg";
+import PinkTag from "./IconPinkTag.svg";
+import BlueTag from "./IconBlueTag.svg";
+import DarkOrangeTag from "./IconDarkOrangeTag.svg";
+import PurpleTag from "./IconPurpleTag.svg";
 
 const AffiliateCard = ({
   tagIcon,
   title,
   comments,
   cardBox,
+  cardBoxName,
   boxColor,
   borderColor,
   href,
   imgSrc,
   imgSrcTracking,
 }) => {
+  const getTag = () => {
+    if (tagIcon === "orange") {
+      return OrangeTag;
+    } else if (tagIcon === "pink") {
+      return PinkTag;
+    } else if (tagIcon === "blue") {
+      return BlueTag;
+    } else if (tagIcon === "dark orange") {
+      return DarkOrangeTag;
+    } else if (tagIcon === "purple") {
+      return PurpleTag;
+    }
+  };
   return (
     <div className="box">
       {/* <Header /> */}
       <div className="box--div">
-        <img src={tagIcon} className="box--img" />
+        <img src={getTag()} className="box--img" />
 
         <div className="box--first">
           {/* <a href={purchaseLink} /> */}
@@ -43,15 +62,23 @@ const AffiliateCard = ({
         </div>
       </div>
 
-      <div className="box--second">
+      <div
+        className="box--second"
+        style={{
+          backgroundColor: boxColor,
+          border: borderColor,
+        }}
+      >
         <h1 className="box--title">{title}</h1>
         <h4 className="box--comments">{comments}</h4>
-        <div>{cardBox}</div>
-        <div style={{ backgroundColor: boxColor }}></div>
-        <div style={{ backgroundColor: borderColor }}></div>
         <div className="box--div-cadrBox">
-          <div className="box--cardBox">
-            <h1 className="box--card--name">Hello</h1>
+          <div
+            className="box--cardBox"
+            style={{
+              backgroundColor: cardBox,
+            }}
+          >
+            <h1 className="box--card--name">{cardBoxName}</h1>
           </div>
         </div>
       </div>
--- src/App/AffiliateLinks.jsx
+++ src/App/AffiliateLinks.jsx
@@ -1,21 +1,66 @@
 import AffiliateCard from "./AffiliateCard";
+import Title from "./Title";
 import "./AffiliateCard.css";
+import "./AffiliateLinks.css";
+import Footer from "./Footer";
+import Header from "./Header";
 
-import OrangeTag from "./affiliatelinkTag.svg";
+// import OrangeTag from "./affiliatelinkTag.svg";
 
 const AffiliateLinks = () => {
   return (
-    <div className="affiliate">
-      <AffiliateCard
-        tagIcon={OrangeTag}
-        title="Flight Knight Lightweight 4 Wheel"
-        comments="“Perfect to travel with restriction size. I never had problem with this luggage.”"
-      />
-      {/* <AffiliateCard
-        tagIcon={OrangeTag}
-        title="Flight Knight Lightweight 4 Wheel"
-        comments="“Perfect to travel with restriction size. I never had problem with this luggage.”"
-      /> */}
+    <div>
+      <div className="header">
+        <Header></Header>
+      </div>
+      <Title name="Affiliate Links" />
+      <div className="affiliate">
+        <AffiliateCard
+          tagIcon="orange"
+          title="Flight Knight Lightweight 4 Wheel"
+          comments="“Perfect to travel with restriction size. I never had problem with this luggage.”"
+          boxColor="#C976EE"
+          borderColor="3px solid #A53AFA"
+          cardBox="#DABEFE"
+          cardBoxName="gimbal"
+        />
+        <AffiliateCard
+          tagIcon="pink"
+          title="Flight Knight Lightweight 4 Wheel"
+          comments="“Perfect to travel with restriction size. I never had problem with this luggage.”"
+          boxColor="#F175E1"
+          borderColor="3px solid #D34EC2"
+          cardBox="#FFC7F9"
+          cardBoxName="gimbal"
+        />
+        <AffiliateCard
+          tagIcon="dark orange"
+          title="Flight Knight Lightweight 4 Wheel"
+          comments="“Perfect to travel with restriction size. I never had problem with this luggage.”"
+          boxColor="#F6AE65"
+          borderColor="3px solid #C38C00"
+          cardBox="#FFD77C"
+          cardBoxName="Travel Tips"
+        />
+        <AffiliateCard
+          tagIcon="orange"
+          title="Flight Knight Lightweight 4 Wheel"
+          comments="“Perfect to travel with restriction size. I never had problem with this luggage.”"
+          boxColor="#FFEBBE"
+          borderColor="3px solid #FFD773"
+          cardBox="#FECF57"
+          cardBoxName="Travel Tips"
+        />
+        <AffiliateCard
+          tagIcon="blue"
+          title="Flight Knight Lightweight 4 Wheel"
+          comments="“Perfect to travel with restriction size. I never had problem with this luggage.”"
+          boxColor="#6CBFD3"
+          borderColor="3px solid #057994"
+          cardBox="#9AD9E8"
+          cardBoxName="Travel Tips"
+        />
+      </div>
     </div>
   );
 };
